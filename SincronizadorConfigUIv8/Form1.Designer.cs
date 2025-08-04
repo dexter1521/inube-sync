fdfd594dfe354270ef2e956e70f59fae44ea492e
Eduardo Marvil <emtv2126@gmail.com>
2025-08-04 12:07:36 -0600

    Cambiando diseño

--- SincronizadorConfigUIv8/Form1.Designer.cs
+++ SincronizadorConfigUIv8/Form1.Designer.cs
@@ -47,10 +47,10 @@
 			// 
 			// btnCargar
 			// 
-			btnCargar.Location = new Point(141, 520);
-			btnCargar.Margin = new Padding(4, 5, 4, 5);
+			btnCargar.Location = new Point(123, 390);
+			btnCargar.Margin = new Padding(4, 4, 4, 4);
 			btnCargar.Name = "btnCargar";
-			btnCargar.Size = new Size(100, 35);
+			btnCargar.Size = new Size(88, 26);
 			btnCargar.TabIndex = 0;
 			btnCargar.Text = "Cargar";
 			btnCargar.UseVisualStyleBackColor = true;
@@ -59,60 +59,61 @@
 			// label1
 			// 
 			label1.AutoSize = true;
-			label1.Location = new Point(47, 55);
+			label1.Location = new Point(41, 41);
 			label1.Margin = new Padding(4, 0, 4, 0);
 			label1.Name = "label1";
-			label1.Size = new Size(61, 20);
+			label1.Size = new Size(49, 15);
 			label1.TabIndex = 1;
 			label1.Text = "URL API";
 			// 
 			// txtApiUrl
 			// 
-			txtApiUrl.Location = new Point(141, 55);
-			txtApiUrl.Margin = new Padding(4, 5, 4, 5);
+			txtApiUrl.Location = new Point(123, 41);
+			txtApiUrl.Margin = new Padding(4, 4, 4, 4);
 			txtApiUrl.Name = "txtApiUrl";
-			txtApiUrl.Size = new Size(471, 27);
+			txtApiUrl.Size = new Size(413, 23);
 			txtApiUrl.TabIndex = 2;
 			// 
 			// txtApiUser
 			// 
-			txtApiUser.Location = new Point(141, 111);
-			txtApiUser.Margin = new Padding(4, 5, 4, 5);
+			txtApiUser.Location = new Point(123, 83);
+			txtApiUser.Margin = new Padding(4, 4, 4, 4);
 			txtApiUser.Name = "txtApiUser";
-			txtApiUser.Size = new Size(471, 27);
+			txtApiUser.Size = new Size(413, 23);
 			txtApiUser.TabIndex = 3;
 			// 
 			// txtApiPassword
 			// 
-			txtApiPassword.Location = new Point(141, 178);
-			txtApiPassword.Margin = new Padding(4, 5, 4, 5);
+			txtApiPassword.Location = new Point(123, 115);
+			txtApiPassword.Margin = new Padding(4, 4, 4, 4);
 			txtApiPassword.MaxLength = 255;
+			txtApiPassword.Multiline = true;
 			txtApiPassword.Name = "txtApiPassword";
-			txtApiPassword.Size = new Size(471, 27);
+			txtApiPassword.Size = new Size(413, 53);
 			txtApiPassword.TabIndex = 4;
 			// 
 			// txtLogDirectory
 			// 
-			txtLogDirectory.Location = new Point(141, 234);
-			txtLogDirectory.Margin = new Padding(4, 5, 4, 5);
+			txtLogDirectory.Location = new Point(123, 176);
+			txtLogDirectory.Margin = new Padding(4, 4, 4, 4);
 			txtLogDirectory.Name = "txtLogDirectory";
-			txtLogDirectory.Size = new Size(471, 27);
+			txtLogDirectory.Size = new Size(413, 23);
 			txtLogDirectory.TabIndex = 5;
 			// 
 			// txtConnectionString
 			// 
-			txtConnectionString.Location = new Point(141, 298);
-			txtConnectionString.Margin = new Padding(4, 5, 4, 5);
+			txtConnectionString.Location = new Point(123, 224);
+			txtConnectionString.Margin = new Padding(4, 4, 4, 4);
 			txtConnectionString.Name = "txtConnectionString";
-			txtConnectionString.Size = new Size(471, 27);
+			txtConnectionString.Size = new Size(413, 23);
 			txtConnectionString.TabIndex = 6;
 			// 
 			// btnGuardar
 			// 
-			btnGuardar.Location = new Point(513, 520);
-			btnGuardar.Margin = new Padding(4, 5, 4, 5);
+			btnGuardar.Location = new Point(449, 390);
+			btnGuardar.Margin = new Padding(4, 4, 4, 4);
 			btnGuardar.Name = "btnGuardar";
-			btnGuardar.Size = new Size(100, 35);
+			btnGuardar.Size = new Size(88, 26);
 			btnGuardar.TabIndex = 8;
 			btnGuardar.Text = "Guardar";
 			btnGuardar.UseVisualStyleBackColor = true;
@@ -120,68 +121,68 @@
 			// 
 			// nudIntervalo
 			// 
-			nudIntervalo.Location = new Point(141, 365);
-			nudIntervalo.Margin = new Padding(4, 5, 4, 5);
+			nudIntervalo.Location = new Point(123, 274);
+			nudIntervalo.Margin = new Padding(4, 4, 4, 4);
 			nudIntervalo.Name = "nudIntervalo";
-			nudIntervalo.Size = new Size(472, 27);
+			nudIntervalo.Size = new Size(413, 23);
 			nudIntervalo.TabIndex = 9;
 			nudIntervalo.TextAlign = HorizontalAlignment.Right;
 			// 
 			// label2
 			// 
 			label2.AutoSize = true;
-			label2.Location = new Point(47, 111);
+			label2.Location = new Point(41, 83);
 			label2.Margin = new Padding(4, 0, 4, 0);
 			label2.Name = "label2";
-			label2.Size = new Size(59, 20);
+			label2.Size = new Size(47, 15);
 			label2.TabIndex = 10;
 			label2.Text = "Usuario";
 			// 
 			// label3
 			// 
 			label3.AutoSize = true;
-			label3.Location = new Point(47, 178);
+			label3.Location = new Point(41, 115);
 			label3.Margin = new Padding(4, 0, 4, 0);
 			label3.Name = "label3";
-			label3.Size = new Size(70, 20);
+			label3.Size = new Size(57, 15);
 			label3.TabIndex = 11;
 			label3.Text = "Password";
 			// 
 			// label4
 			// 
 			label4.AutoSize = true;
-			label4.Location = new Point(47, 234);
+			label4.Location = new Point(41, 176);
 			label4.Margin = new Padding(4, 0, 4, 0);
 			label4.Name = "label4";
-			label4.Size = new Size(67, 20);
+			label4.Size = new Size(53, 15);
 			label4.TabIndex = 12;
 			label4.Text = "Dir. Logs";
 			// 
 			// label5
 			// 
 			label5.AutoSize = true;
-			label5.Location = new Point(47, 298);
+			label5.Location = new Point(41, 224);
 			label5.Margin = new Padding(4, 0, 4, 0);
 			label5.Name = "label5";
-			label5.Size = new Size(71, 20);
+			label5.Size = new Size(58, 15);
 			label5.TabIndex = 13;
 			label5.Text = "Conexión";
 			// 
 			// label6
 			// 
 			label6.AutoSize = true;
-			label6.Location = new Point(23, 365);
+			label6.Location = new Point(41, 274);
 			label6.Margin = new Padding(4, 0, 4, 0);
 			label6.Name = "label6";
-			label6.Size = new Size(92, 20);
+			label6.Size = new Size(73, 15);
 			label6.TabIndex = 14;
 			label6.Text = "Tiem de Act.";
 			// 
 			// Form1
 			// 
-			AutoScaleDimensions = new SizeF(8F, 20F);
+			AutoScaleDimensions = new SizeF(7F, 15F);
 			AutoScaleMode = AutoScaleMode.Font;
-			ClientSize = new Size(704, 608);
+			ClientSize = new Size(616, 456);
 			Controls.Add(label6);
 			Controls.Add(label5);
 			Controls.Add(label4);
@@ -196,7 +197,7 @@
 			Controls.Add(txtApiUrl);
 			Controls.Add(label1);
 			Controls.Add(btnCargar);
-			Margin = new Padding(4, 5, 4, 5);
+			Margin = new Padding(4, 4, 4, 4);
 			Name = "Form1";
 			StartPosition = FormStartPosition.CenterScreen;
 			Text = "Inube v1";
